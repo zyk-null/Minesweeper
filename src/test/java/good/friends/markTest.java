0296206a38e19adca9cdee97639c2d75bb74ef65
zyk-null <3113688006@qq.com>
2023-04-05 23:02:17 +0800

    完成最后一个，在我这里都能通过测试

--- src/test/java/good/friends/markTest.java
+++ src/test/java/good/friends/markTest.java
@@ -14,30 +14,61 @@ public class markTest {
     public void testMark() throws Exception {
         Method method=ms.getClass().getDeclaredMethod("mark",int.class,int.class);
         method.setAccessible(true);
-        Field field=ms.getClass().getDeclaredField("show");
-        field.setAccessible(true);
-        char[][] show=(char[][])field.get(ms);
-        field=ms.getClass().getDeclaredField("mine");
-        field.setAccessible(true);
-        int[][] mine=(int[][])field.get(ms);
-        field=ms.getClass().getDeclaredField("count");
-        field.setAccessible(true);
-        int[][] count=(int[][])field.get(ms);
-        //测试标记雷的情况
+        Field field_show=ms.getClass().getDeclaredField("show");
+        field_show.setAccessible(true);
+        char[][] show=(char[][])field_show.get(ms);
+        Field field_mine=ms.getClass().getDeclaredField("mine");
+        field_mine.setAccessible(true);
+        int[][] mine=(int[][])field_mine.get(ms);
+        Field field_flagNum=ms.getClass().getDeclaredField("flagNum");
+        field_flagNum.setAccessible(true);
+        int flagNum=(int)field_flagNum.get(ms);
+        Field field_hitNum=ms.getClass().getDeclaredField("hitNum");
+        field_hitNum.setAccessible(true);
+        int hitNum=(int)field_hitNum.get(ms);
+
+
+        //标记到正确的雷，flagNum+1，hitNum+1，show变为*
+        mine[1][1]=1;
         method.invoke(ms,1,1);
-        assertEquals('*',show[1][1]);
-        //测试标记非雷的情况
-        method.invoke(ms,1,2);
-        assertEquals('?',show[1][2]);
-        //测试标记已标记的情况
+        flagNum=(int)field_flagNum.get(ms);
+        hitNum=(int)field_hitNum.get(ms);
+        assertEquals(1,flagNum);
+        assertEquals(1,hitNum);
+        assertEquals('?',show[1][1]);
+        //标记到已经标记过的雷格子，flagNum-1，hitNum-1，show变为*
         method.invoke(ms,1,1);
+        flagNum=(int)field_flagNum.get(ms);
+        hitNum=(int)field_hitNum.get(ms);
+        assertEquals(0,flagNum);
+        assertEquals(0,hitNum);
         assertEquals('*',show[1][1]);
-        //测试标记已标记的情况
-        method.invoke(ms,1,2);
-        assertEquals('*',show[1][2]);
-        //测试标记已打开的情况
-        show[1][3]='1';
-        method.invoke(ms,1,3);
-        assertEquals('1',show[1][3]);
+
+
+        //标记到错误的雷，flagNum+1，show变为*
+        mine[2][2]=0;
+        method.invoke(ms,2,2);
+        flagNum=(int)field_flagNum.get(ms);
+        hitNum=(int)field_hitNum.get(ms);
+        assertEquals(1,flagNum);
+        assertEquals(0,hitNum);
+        assertEquals('?',show[2][2]);
+        //标记到已经标记过的非雷格子，flagNum-1，show变为*
+        method.invoke(ms,2,2);
+        flagNum=(int)field_flagNum.get(ms);
+        hitNum=(int)field_hitNum.get(ms);
+        assertEquals(0,flagNum);
+        assertEquals(0,hitNum);
+        assertEquals('*',show[2][2]);
+        //标记到已经打开的格子，flagNum不变，show不变
+        show[3][3]='1';
+        method.invoke(ms,3,3);
+        flagNum=(int)field_flagNum.get(ms);
+        hitNum=(int)field_hitNum.get(ms);
+        assertEquals(0,flagNum);
+        assertEquals(0,hitNum);
+        assertEquals('1',show[3][3]);
+
+
     }
 }
